8f919d63dc51bf7c2d13efcc2e7e1b0d52a979d0
Bukhmastov Alex <bukhmastov-alex@ya.ru>
2017-04-13 16:43:22 +0300

    Added wuwari easter egg (instead half of pika:D)

--- app/src/main/java/com/bukhmastov/cdoitmo/activities/PikaActivity.java
+++ app/src/main/java/com/bukhmastov/cdoitmo/activities/PikaActivity.java
@@ -3,16 +3,24 @@ package com.bukhmastov.cdoitmo.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.bukhmastov.cdoitmo.R;
 
+import java.util.Random;
+
 public class PikaActivity extends AppCompatActivity {
+
+    private final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pika);
         overridePendingTransition(R.anim.zoom_bottom_in, R.anim.zoom_bottom_out);
+        if (random.nextInt(5) % 5 == 0) {
+            ((ImageView) findViewById(R.id.image)).setImageDrawable(getDrawable(R.drawable.wuwari));
+        }
         View pika_container = findViewById(R.id.pika_container);
         if (pika_container != null) {
             pika_container.setOnClickListener(new View.OnClickListener() {
