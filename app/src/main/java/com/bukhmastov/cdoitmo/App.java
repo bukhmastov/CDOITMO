23c80219bd2efb2396dd64039af5746d79284845
Bukhmastov Alex <bukhmastov-alex@ya.ru>
2017-11-26 16:37:53 +0300

    Backward compatibility for cached schedules

--- app/src/main/java/com/bukhmastov/cdoitmo/App.java
+++ app/src/main/java/com/bukhmastov/cdoitmo/App.java
@@ -54,34 +54,6 @@ public class App extends Application {
 }
 
 /*
- * --------------
- * TODO ИЗМЕНЕНИЯ - Обеспечение обратной совместимости | переход на версию 2.0
- * --------------
- * Раписание:
- *      * Корень json переработан
- *          OLD: query, type, title, label, pid, timestamp, cache_token, schedule
- *          NEW: schedule_type, query, type, title, timestamp, schedule
- * Расписание занятий:
- *      * Изменения в дне
- *          index -> weekday
- * Расписание экзаменов:
- *      * Экземпляр экзамена переработан
- *          OLD: subject, teacher, exam{date, time, room}, consult{date, time, room}
- *          NEW: subject, group, teacher, teacher_id, exam{date, time, room, building}, advice{date, time, room, building}
- * Скрытые и добавленные занятия:
- *      * Изменения в дне
- *          index -> weekday
- * Шаринг занятий
- *      * Изменение структуры
- *          OLD: type, content{query, title, token, added, reduced}
- *          NEW: type, version, content{query, title, added, reduced}
- * Кэш расписаний (любых) именуется query.toLowerCase() вместо [type + "_" + query]
- *
- *
- *
- */
-
-/*
  * -------------
  * TODO ROAD MAP
  * -------------
